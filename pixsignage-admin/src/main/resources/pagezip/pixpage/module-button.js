dfd36c4811d19ad72a9504a965213fd20bbd2630
Bright Deng <bright.deng@videoexpress.biz>
2017-12-01 10:27:21 +0800

    fix bugs

---
+++ pixsignage-admin/src/main/resources/pagezip/pixpage/module-button.js
@@ -0,0 +1,141 @@
+var ButtonZone = function (zonediv, zone) {
+	this.zonediv = zonediv;
+	this.zone = zone;
+
+	var init = function () {
+		var pagezonedtls = zone.pagezonedtls;
+		if (pagezonedtls.length > 0 && pagezonedtls[0].image != null) {
+			if (PixPage.mode == 'preview') {
+				$(zonediv).css('background-image', 'url(/pixsigdata' + pagezonedtls[0].image.filepath + ')');
+			} else {
+				$(zonediv).css('background-image', 'url(image/' + pagezonedtls[0].image.filename + ')');
+			}
+			$(zonediv).css('background-size', '100% 100%');
+			$(zonediv).css('background-position', 'center');
+			$(zonediv).css('background-repeat', 'no-repeat');
+		}
+		var a_element = document.createElement('a');
+		if (zone.touchtype == 0) {
+			if (PixPage.mode != 'preview') {
+				if (typeof(TeaTableAndroidBridge) == 'undefined') {
+					a_element.href = 'javascript:history.back(-1)';
+				} else {
+					a_element.href = 'javascript:TeaTableAndroidBridge.backtohome()';
+				}
+			}
+		} else if (zone.touchtype == 1) {
+			if (PixPage.mode != 'preview') {
+				if (typeof(TeaTableAndroidBridge) == 'undefined') {
+					a_element.href = 'index.html';
+				} else {
+					a_element.href = 'javascript:TeaTableAndroidBridge.backtohome()';
+				}
+			}
+		} else if (zone.touchtype == 2) {
+			if (PixPage.mode != 'preview') {
+				a_element.href = zone.touchpageid + '.html';
+			}
+		} else if (zone.touchtype == 3) {
+			$(a_element).attr('href', 'javascript:;');
+			$(a_element).attr('diyaction', zone.diyaction.code);
+			$(a_element).click(function(e) {
+				if (PixPage.diymodules.length > 0) {
+					PixPage.diymodules[0].doAction($(a_element).attr('diyaction'));
+				}
+			});
+		}
+		$(zonediv).wrap(a_element);
+		var p_element = document.createElement('p');
+		$(p_element).html(zone.content);
+		$(zonediv).append(p_element);
+		
+		if (zone.fixflag == 0) {
+			var myElement = document.getElementById('PagezoneDiv' + zone.pagezoneid);
+			var mc = new Hammer.Manager(myElement);
+			var pan = new Hammer.Pan();
+			var pinch = new Hammer.Pinch();
+			var rotate = new Hammer.Rotate();
+			pinch.recognizeWith(rotate);
+			mc.add([pan, pinch, rotate]);
+			
+			var lastPosX = 0, lastPosY = 0, posX = 0, posY = 0;
+			var scale = 1, last_scale = 1;
+			var rotation = 0, last_rotation = 0, start_rotation = 0;
+			var zindex = zone.zindex;
+			mc.on('pan panend pinchstart pinch pinchend rotatestart rotate rotateend tap multitap', function(e) {
+				e.preventDefault();
+				if (zindex < PixPage.maxzindex) {
+					PixPage.maxzindex ++;
+					zindex = PixPage.maxzindex;
+					$('#PagezoneDiv' + zone.pagezoneid).css('z-index', zindex);
+					$('#PagezoneDiv' + zone.pagezoneid + ' p').html(zone.content);
+				}
+				switch (e.type) {
+					case 'pan':
+						posX = e.deltaX + lastPosX;
+						posY = e.deltaY + lastPosY;
+						break;
+					case 'panend':
+						lastPosX = posX;
+						lastPosY = posY;
+						break;
+					case 'pinchstart':
+						last_scale = scale;
+						break;
+					case 'pinch':
+						scale = Math.max(1, Math.min(last_scale * e.scale, 10));
+						break;
+					case 'pinchend':
+						last_scale = scale;
+						break;
+					case 'rotatestart':
+						last_scale = scale;
+						last_rotation = rotation;
+						start_rotation = e.rotation;
+						break;
+					case 'rotate':
+						rotation = last_rotation - start_rotation + e.rotation;
+						break;
+					case 'rotateend':
+						last_scale = scale;
+						last_rotation = rotation;
+						break;
+				}
+				$('#PagezoneDiv' + zone.pagezoneid).css('-webkit-transform', 'translate3d(' + posX +'px,' + posY + 'px, 0 ) rotate(' + rotation + 'deg)' + ' scale3d(' + scale + ', ' + scale + ', 1 )');
+			});
+		}
+	};
+
+	this.resize = function (scalew, scaleh) {
+		var shadow = '';
+		shadow += (parseInt(zone.shadowh) / scalew) + 'px ';
+		shadow += (parseInt(zone.shadowv) / scaleh) + 'px ';
+		shadow += (parseInt(zone.shadowblur) / scalew) + 'px ';
+		shadow += zone.shadowcolor;
+		$(zonediv).css({
+			'box-sizing': 'border-box',
+			'border-color': zone.bdcolor, 
+			'border-style': zone.bdstyle, 
+			'border-width': Math.ceil(parseInt(zone.bdwidth) / scalew) + 'px', 
+			'border-radius': Math.ceil(parseInt(zone.bdradius) / scalew) + 'px', 
+			'color': zone.color, 
+			'font-family': zone.fontfamily, 
+			'font-size': Math.ceil(parseInt(zone.fontsize) / scalew) + 'px', 
+			'text-decoration': zone.decoration, 
+			'text-align': zone.align, 
+			'font-weight': zone.fontweight, 
+			'font-style': zone.fontstyle, 
+			'line-height': Math.ceil(parseInt(zone.lineheight) / scaleh) + 'px', 
+			'text-shadow': shadow, 
+			'word-wrap': 'break-word',
+		});
+		$(zonediv).find('p').css({
+			'word-wrap': 'break-word',
+			'white-space': 'pre-wrap',
+			'text-decoration': zone.decoration,
+			'margin': '0 0 0px',
+		});
+	};
+	
+	init();
+};
---
+++ pixsignage-admin/src/main/webapp/preview/pixpage/pixpage.js
@@ -0,0 +1,172 @@
+var PixPage = function () {
+	var allmodules = [];
+	var diymodules = [];
+	var maxzindex = 0;
+
+	var init = function (mode) {
+		this.mode = mode;
+		$('#PageDiv').empty();
+		var width = $('#PageDiv').width();
+		var height = $('#PageDiv').height();
+		var scalew = Page.width / width;
+		var scaleh = Page.height / height;
+		var zones = Page.pagezones;
+		var animations = [];
+		for (var i = 0; i < zones.length; i++) {
+			var zone = zones[i];
+			var zone_div = document.createElement('div');
+			PixPage.maxzindex = Math.max(PixPage.maxzindex, zone.zindex + 1);
+			zone_div.id = 'PagezoneDiv' + zone.pagezoneid;
+			$(zone_div).attr('zoneid', zone.pagezoneid);
+			$(zone_div).attr('zonetype', zone.type);
+			var background_div = document.createElement('div');
+			background_div.id = 'PagezoneBG';
+			var inner_div = document.createElement('div');
+			inner_div.id = 'PagezoneCT';
+			$(zone_div).append(background_div);
+			$(zone_div).append(inner_div);
+			$('#PageDiv').append(zone_div);
+			
+			if (zone.type == 1) {
+				var videozone = new VideoZone($(inner_div), zone);
+				allmodules.push(videozone);
+			} else if (zone.type == 2) {
+				var imagezone = new ImageZone($(inner_div), zone);
+				allmodules.push(imagezone);
+			} else if (zone.type == '3') {
+				//Text Zone
+				var textzone = new TextZone($(inner_div), zone);
+				allmodules.push(textzone);
+			} else if (zone.type == '4') {
+				//Scroll Zone
+				var scrollzone = new ScrollZone($(inner_div), zone);
+				allmodules.push(scrollzone);
+			} else if (zone.type == '5') {
+				//Date Zone
+				var datezone = new DateZone($(inner_div), zone);
+				allmodules.push(datezone);
+			} else if (zone.type == '6') {
+				//Web Zone
+				var webzone = new WebZone($(inner_div), zone);
+				allmodules.push(webzone);
+			} else if (zone.type == '7') {
+				//Button Zone
+				var buttonzone = new ButtonZone($(inner_div), zone);
+				allmodules.push(buttonzone);
+			} else if (zone.type == '11') {
+				//CalendarList Zone
+				var dczone = new DailyCourse($(inner_div), zone, scalew, scaleh);
+				allmodules.push(dczone);
+			} else if (zone.type == '12') {
+				//CalendarTable Zone
+				var wczone = new WeeklyCourse($(inner_div), zone, scalew, scaleh);
+				allmodules.push(wczone);
+			} else if (zone.type == '13') {
+				//Attendance Zone
+				var attendance = new Attendance($(inner_div), zone, scalew, scaleh);
+				allmodules.push(attendance);
+			} else if (zone.type == '14') {
+				//HomeSchool Zone
+				var homeschool = new HomeSchool($(inner_div), zone, scalew, scaleh);
+				allmodules.push(homeschool);
+			} else if (zone.type == '15') {
+				//ExamNotice Zone
+				var examnotice = new ExamNotice($(inner_div), zone, scalew, scaleh);
+				allmodules.push(examnotice);
+			} else if (zone.type == '21') {
+				//Diy Zone
+				if (zone.diy.type == 'route-guide') {
+					var diypath = './' + zone.diy.code;
+					if (PixPage.mode == 'preview') {
+						diypath = '/pixsigdata/diy/' + zone.diy.code;
+					}
+					var routeguide = new RouteGuide($(inner_div), zone, scalew, scaleh, diypath);
+					allmodules.push(routeguide);
+					diymodules.push(routeguide);
+				}
+			} else if (zone.type == '31') {
+				//Meeting Zone
+				var meeting = new Meeting($(inner_div), zone, scalew, scaleh);
+				allmodules.push(meeting);
+			} else {
+				var otherzone = new OtherZone($(inner_div), zone);
+				allmodules.push(otherzone);
+			}
+			
+			if (zone.animationinit != 'none' && zone.animationinit != '') {
+				var animation = {
+					onevent: 'load',
+					selectors: '#PagezoneDiv' + zone.pagezoneid + ' #PagezoneCT',
+					anid: zone.animationinit,
+					delay: zone.animationinitdelay + 'ms',
+					iterationcount: -1,
+				};
+				animations.push(animation);
+				console.log('Add on init animation: ', zone.pagezoneid, zone.animationinit);
+			}
+			if (zone.animationclick != 'none' && zone.animationinit != '') {
+				var animation = {
+					onevent: 'click',
+					selectors: '#PagezoneDiv' + zone.pagezoneid + ' #PagezoneCT',
+					anid: zone.animationclick,
+					delay: 0,
+					iterationcount: 1,
+				};
+				animations.push(animation);
+				console.log('Add on click animation: ', zone.pagezoneid, zone.animationinit);
+			}
+		}
+		
+		resize();
+		
+		var anCapacity = new AnCapacity();
+		anCapacity.init(null, animations);
+	};
+
+	var resize = function () {
+		var width = $('#PageDiv').width();
+		var height = $('#PageDiv').height();
+		var scalew = Page.width / width;
+		var scaleh = Page.height / height;
+		for (var i = 0; i < allmodules.length; i++) {
+			var zone = allmodules[i].zone;
+			var zone_div = $('#PagezoneDiv' + zone.pagezoneid);
+			var background_div = $(zone_div).find('#PagezoneBG');
+			var inner_div = $(zone_div).find('#PagezoneCT');		
+			$(zone_div).css({
+				'position': 'absolute',
+				'width': 100*zone.width/Page.width + '%',
+				'height': 100*zone.height/Page.height + '%',
+				'top': 100*zone.topoffset/Page.height + '%', 
+				'left': 100*zone.leftoffset/Page.width + '%', 
+				'z-index': zone.zindex,
+				'-moz-transform': zone.transform,
+				'-webkit-transform': zone.transform,
+			});
+			$(background_div).css({
+				'position': 'absolute',
+				'width': '100%',
+				'height': '100%',
+				'background': zone.bgcolor, 
+				'opacity': parseInt(zone.bgopacity)/255, 
+			});
+			$(inner_div).css({
+				'position': 'absolute',
+				'height': '100%', 
+				'width': '100%', 
+				'padding': (parseInt(zone.padding) / scalew) + 'px', 
+			});
+			allmodules[i].resize(scalew, scaleh);
+		}
+	};
+	
+	return {
+		init: init,
+		resize: resize,
+		diymodules: diymodules,
+		maxzindex: maxzindex,
+	}
+	
+}();
+
+
